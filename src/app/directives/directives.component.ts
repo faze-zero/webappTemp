d2b55ee5bd78411c14bf1b8f6ceb88695ba82c1e
Ali Abou Abbas <ali.abbas@live.ca>
2017-02-26 19:18:58 -0300

    highlight component adding function

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HighlightDirective } from './highlight.directive'
 @Component({
   selector: 'f0-directives',
   templateUrl: './directives.component.html',
